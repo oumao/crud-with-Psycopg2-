ad3abf228a1eca39c3d80bf9618dfa1ae88d094c
oumao <evansotis2015@gmail.com>
2018-11-02 17:04:51 +0300

    try to fix addUser function

--- new.py
+++ new.py
@@ -1,34 +1,37 @@
 import os
 import psycopg2
 
+# creating connection to the postgres database using psycopg2 library
 con = psycopg2.connect(host='localhost', user='postgres',
-                       password='oumaruc2016', port=5400, dbname='futchat')
+                       password='oumaruc2016', port=5400, dbname='futgen')
 curs = con.cursor()
 
-def createUser(con=None):
+# create table users
+
+
+def createTable(con=None):
     try:
         if con == None:
             return False
-        sql = """create table users if doesnot exist(id serial not null,
-                                                     username text not null, 
-                                                     password varchar not null)"""
-        curs.execute(sql)
+        curs.execute(
+            "create table users(id serial primary key not null, username character varying(200) not null, password integer not null)")
         con.commit()
         con.close()
     except Exception as err:
-        return ('Couldnot create user',err)  
+        raise err
 
-def addUser(name, passw):
+# function to add users in the table users
+
+
+def addUser(id, username, password):
     try:
-        if con == None:
-            return False
-        curs.execute("""INSERT INTO users values({0},{1})""".format(name, passw))
+        curs.execute("insert into users(id, username, password) values(?, ?, ?)", (
+            id, username, password))
         con.commit()
-        con.close()
-    except Exception as e:
-        raise e
-        
+        con.rollback()
+    except Exception as err:
+        raise err
+
+
 if __name__ == '__main__':
-    # createUser(con)
-     addUser('Ouma', 'tom123')
-   
+    addUser(3, 'John', 678909)
